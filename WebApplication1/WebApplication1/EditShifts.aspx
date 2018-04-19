7f8e9c3f9371e395b31b263e731aa8f2cd48eb54
= <=>
2018-04-18 21:45:55 -0500

    EditShifts

---
+++ WebApplication1/WebApplication1/EditShifts.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EditShifts.aspx.cs" Inherits="WebApplication1.EditShifts" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:GridView ID="GridViewShift" runat="server" AllowPaging="True" CellPadding="0"
+                      GridLines="None" AutoGenerateColumns="False" ShowFooter="True" AllowSorting="True"
+                      ForeColor="#333333" PageSize="15" Width="1200px" DataKeyNames="ShiftID" HorizontalAlign="Center"
+                      OnRowCancelingEdit="GridViewShift_RowCancelingEdit"
+                    OnRowEditing="GridViewShift_RowEditing" OnRowUpdating="GridViewShift_RowUpdating"
+                    OnRowDeleting="GridViewShift_RowDeleting" OnRowDataBound="GridViewShift_RowDataBound"
+                    OnPageIndexChanging="GridViewShift_PageIndexChanging">
+            <EditRowStyle BackColor="#7C6F57" />
+            <FooterStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" HorizontalAlign="Left" />
+            <HeaderStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" HorizontalAlign="Center" />
+            <PagerStyle BackColor="#666666" ForeColor="White" HorizontalAlign="Center" />
+            <AlternatingRowStyle BackColor="White" />
+            <RowStyle HorizontalAlign="Center" Height="35px" VerticalAlign="Middle" />
+            <Columns>
+                <asp:CommandField ShowDeleteButton="True" ShowEditButton="True" />
+                <asp:BoundField DataField="ShiftID" HeaderText="ShiftID" InsertVisible="False" ReadOnly="True" SortExpression="ShiftID" />
+                <asp:BoundField DataField="UserID" HeaderText="UserID" SortExpression="UserID" />
+                <asp:BoundField DataField="Date" HeaderText="Date" SortExpression="Date" />
+                <asp:BoundField DataField="TimeIn" HeaderText="TimeIn" SortExpression="TimeIn" />
+                <asp:BoundField DataField="TimeOut" HeaderText="TimeOut" SortExpression="TimeOut" />
+                <asp:BoundField DataField="TimeWorked" HeaderText="TimeWorked" SortExpression="TimeWorked" />
+                <asp:BoundField DataField="RoundedTimeWorked" HeaderText="RoundedTimeWorked" SortExpression="RoundedTimeWorked" />
+                <asp:BoundField DataField="Comments" HeaderText="Comments" SortExpression="Comments" />
+            </Columns>
+            <SelectedRowStyle ForeColor="#333333" Font-Bold="True" BackColor="#C5BBAF"></SelectedRowStyle>
+            <RowStyle BackColor="#E3EAEB"></RowStyle>
+            <SortedAscendingCellStyle BackColor="#F8FAFA" />
+            <SortedAscendingHeaderStyle BackColor="#246B61" />
+            <SortedDescendingCellStyle BackColor="#D4DFE1" />
+            <SortedDescendingHeaderStyle BackColor="#15524A" />
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebApplication1/EditShifts.aspx.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class EditShifts : System.Web.UI.Page
+    {
+        webtimeclockEntities db;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                db = new webtimeclockEntities();
+                BindGridView();
+            }
+
+        }
+
+        protected void GridViewShift_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridViewShift.EditIndex = -1;
+            BindGridView();
+        }
+
+        protected void GridViewShift_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridViewShift.EditIndex = e.NewEditIndex;
+            BindGridView();
+        }
+
+        protected void GridViewShift_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            db = new webtimeclockEntities();
+
+            try
+            {
+                string shiftID = GridViewShift.Rows[e.RowIndex].Cells[1].Text;
+                string userID = ((TextBox)GridViewShift.Rows[e.RowIndex].Cells[2].Controls[0]).Text;
+                DateTime date = Convert.ToDateTime(((TextBox)GridViewShift.Rows[e.RowIndex].Cells[3].Controls[0]).Text).Date;
+                DateTime timeIn = Convert.ToDateTime(((TextBox)GridViewShift.Rows[e.RowIndex].Cells[4].Controls[0]).Text);
+                DateTime timeOut = Convert.ToDateTime(((TextBox)GridViewShift.Rows[e.RowIndex].Cells[5].Controls[0]).Text);
+                DateTime timeWorked = Convert.ToDateTime(((TextBox)GridViewShift.Rows[e.RowIndex].Cells[6].Controls[0]).Text);
+                DateTime roundedTimeWorked = Convert.ToDateTime(((TextBox)GridViewShift.Rows[e.RowIndex].Cells[7].Controls[0]).Text);
+                string comments = ((TextBox)GridViewShift.Rows[e.RowIndex].Cells[8].Controls[0]).Text;
+
+                int sInt = Convert.ToInt32(shiftID);
+
+                var sh = (from shift in db.shifts
+                          where shift.ShiftID == sInt
+                          select shift).Single();
+
+                sh.UserID = userID;
+                sh.Date = date.Date;
+                sh.TimeIn = timeIn;
+                sh.TimeOut = timeOut;
+                sh.TimeWorked = timeWorked.TimeOfDay;
+                sh.RoundedTimeWorked = roundedTimeWorked.TimeOfDay;
+                sh.Comments = comments;
+
+                db.SaveChanges();
+
+                GridViewShift.EditIndex = -1;
+                BindGridView();
+            }
+            catch (Exception ex)
+            {
+                //TODO: write the thing to do the stuff with the prompt to do the datetime correctly like 
+                //format: "mm/dd/yyyy hh:mm:ss (A or P)M"
+            }
+        }
+
+        protected void GridViewShift_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            db = new webtimeclockEntities();
+
+            string shiftID = GridViewShift.Rows[e.RowIndex].Cells[1].Text;
+            int s = Convert.ToInt32(shiftID);
+
+            try
+            {
+                var sh = (from shift in db.shifts
+                          where shift.ShiftID == s
+                          select shift).Single();
+
+                db.shifts.Remove(sh);
+                db.SaveChanges();
+
+                BindGridView();
+            }
+            catch (Exception ex)
+            {
+                //no
+            }
+        }
+
+        protected void GridViewShift_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+
+        }
+
+        protected void GridViewShift_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridViewShift.PageIndex = e.NewPageIndex;
+            BindGridView();
+        }
+
+        private void BindGridView()
+        {
+            db = new webtimeclockEntities();
+
+            var shift = from s in db.shifts
+                        select s;
+
+            if (shift.Count() > 0)
+            {
+                GridViewShift.DataSource = shift.ToList();
+                GridViewShift.DataBind();
+            }
+        }
+    }
+}
